abaf5ae4bac271a78983a902e61abd847ea01717
segin-GH <segin.bytes@gmail.com>
2023-09-08 20:48:09 +0530

    .

---
+++ hello/hello.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('form').addEventListener('submit', function (event) {
+        let name = document.querySelector('#name').value;
+        alert('hello, ' + name);
+        event.preventDefault();
+    });
+});
