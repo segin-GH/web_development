350c9de6835e7a18217036b88481465f9c335451
segin-GH <segin.bytes@gmail.com>
2023-09-09 19:54:27 +0530

    added basic flask app

---
+++ hello_using_flask/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    if "name" in request.args:
+        name = request.args["name"]
+    else:
+        name = "World"
+    return render_template("index.html", name=name)
