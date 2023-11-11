3815e84a60b8063288ec2d7554089d9b3456641e
segin-GH <segin.bytes@gmail.com>
2023-11-12 04:22:26 +0530

    chore: added tailwind to web cereal

--- web-cereal/tailwind.config.js
+++ web-cereal/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'clr-text': '#c7c2c2',
+        'clr-background': '#181818',
+        'clr-primary': '#2a292a',
+        'clr-secondary': '#020617',
+        'clr-accent': '#6366f1',
+      },
+
+    },
   },
   plugins: [],
 }
