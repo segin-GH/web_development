761cdefb7dde192e364cc3b600c87e58c5740419
segin-GH <segin.bytes@gmail.com>
2023-11-13 03:14:57 +0530

    feat: added a content area where the data will be loadded

---
+++ web-cereal/script.js
@@ -0,0 +1,16 @@
+function loadContent(page) {
+  fetch(`${page}.html`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
+    .then(html => {
+      document.getElementById('content-area').innerHTML = html;
+    })
+    .catch(error => {
+      console.error('Error loading content:', error);
+      document.getElementById('content-area').innerHTML = '<p>Error loading content.</p>';
+    });
+}
