06a10f8ec4801a969ed98ae18f248866ec0a8c84
segin-GH <segin.bytes@gmail.com>
2023-11-13 01:00:02 +0530

    feat: added 2 nav bars top and left side

--- web-cereal/tailwind.config.js
+++ web-cereal/tailwind.config.js
@@ -4,10 +4,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        'clr-text': '#c7c2c2',
-        'clr-background': '#181818',
-        'clr-primary': '#2a292a',
-        'clr-secondary': '#020617',
+        'clr-txt': '#c7c2c2',
+        'clr-bg-prim': '#181818',
+        'clr-bg-sec': '#171717',
+        'clr-bg-thr': '#1c1c1e',
+        'clr-prim': '#2a292a',
+        'clr-sec': '#020617',
         'clr-accent': '#6366f1',
       },
 
