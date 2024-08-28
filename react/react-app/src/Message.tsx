121529a6186e18124d31fc0d4eaba670970d467a
segin-GH <segin.bytes@gmail.com>
2024-08-28 20:31:49 +0530

    added a components

--- react/react-app/src/Message.tsx
+++ react/react-app/src/Message.tsx
@@ -1,5 +1,9 @@
 function Message() {
-  return <h1>Hello World</h1>;
+  const name = 'Segin';
+  if (name)
+    return <h1>Hello {name} </h1>;
+  else
+    return <h1>Hello world </h1>;
 }
 
 export default Message;
---
+++ react/react-app/src/components/ListGroup.tsx
@@ -0,0 +1,23 @@
+
+function ListGroup() {
+  const items = [
+    'New York',
+    'San Franciso',
+    'Tokyo',
+    'London',
+    'Paris',
+  ];
+
+
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (<li key={item}>{item}</li>))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
