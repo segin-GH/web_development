9f691765575e501bf0d141d2a83fcd8bf08d4f14
segin-GH <segin.bytes@gmail.com>
2024-08-28 21:51:32 +0530

    using props we can pass data to comp

--- react/react-app/src/components/ListGroup.tsx
+++ react/react-app/src/components/ListGroup.tsx
@@ -1,21 +1,31 @@
+import { useState } from "react";
 
-function ListGroup() {
-  const items = [
-    'New York',
-    'San Franciso',
-    'Tokyo',
-    'London',
-    'Paris',
-  ];
+interface Props {
+  items: string[];
+  heading: string;
+}
 
+function ListGroup({ items, heading }: Props) {
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+
+      {items.length === 0 && <p>There are no items in the list.</p>}
+
       <ul className="list-group">
-        {items.map((item) => (<li key={item}>{item}</li>))}
-      </ul>
+        {items.map((item, index) => (
+          <li
+            className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+            key={item}
+            onClick={() => { setSelectedIndex(index); }}
+
+          >{item}
+          </li>))}
+      </ul >
     </>
   );
 }
