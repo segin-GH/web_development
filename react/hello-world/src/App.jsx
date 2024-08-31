6d7e33d3874afb139f5779fbda8126d496674bc4
segin-GH <segin.bytes@gmail.com>
2024-08-31 23:55:58 +0530

    simple react app

--- react/hello-world/src/App.jsx
+++ react/hello-world/src/App.jsx
@@ -1,6 +1,16 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer";
+import Food from "./components/Food";
 
 function App() {
-  return <div className="App"> Hello world </div>
+  return (
+    <>
+      <Header />
+      <Food />
+      <Footer />
+    </>
+  );
+
 }
 
 export default App
